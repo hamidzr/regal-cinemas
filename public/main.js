33904c04bdaba67180679189b49bdb5c6cee54cd
Hamid Zare <dellydela@gmail.com>
2018-07-29 15:55:11 -0500

    added simple ui to fetch seats info for a movie/time

--- public/main.js
+++ public/main.js
@@ -1,6 +1,40 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    isFetchingSeats: false,
+    theaterId: '',
+    seats: [],
+  },
+
+  methods: {
+    async getSeats() {
+      this.isFetchingSeats = true;
+      if (!this.validateInput()) throw new Error(`invalid theaterId ${this.theaterId}`);
+      let seats = await this.fetchSeats(this.theaterId);
+      this.isFetchingSeats = false;
+      this.seats = seats;
+    },
+
+    async fetchSeats(id) {
+      let { data: seats } = await axios({
+        url: `/theaters/${id}/seats`,
+        method: 'get',
+      });
+      return seats;
+    },
+
+    // cleans and validates input
+    validateInput() {
+      let curInput = this.theaterId;
+      try {
+        let id = curInput.match(/\d{7,11}/)[0];
+        if (id.length.length < 7) return false;
+        this.theaterId = id;
+        return true;
+      } catch(e) {
+        return false;
+      }
+    },
+
   }
 })
